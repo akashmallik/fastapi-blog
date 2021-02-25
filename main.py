066627c6a1fa7da56ae7dec35aaa75bd986ef20c
Akash Mallik <toakashmallik@gmail.com>
2021-02-25 22:47:33 +0600

    path and query param

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import uvicorn
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
 
 app = FastAPI()
 
@@ -11,6 +16,14 @@ def index():
 @app.get('/about')
 def about():
     return {'data': 'about page'}
+
+
+@app.get('/blog')
+def blog(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs'}
+    else:
+        return {'data': f'{limit} blogs'}
 
 
 @app.get('/blog/unpublished')
@@ -28,3 +41,18 @@ def show(id: int):
 def comments(id: int):
     # fetch comments of blog with id = id
     return {'data': ['Comments 1', 'comments 2']}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+
+    return {'data': f"Blog is created with {blog.title}."}
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
