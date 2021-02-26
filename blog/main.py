72377e4160769b4c0bc11a19f09c5ba3a57af747
Akash Mallik <toakashmallik@gmail.com>
2021-02-26 22:04:41 +0600

    status code issue fixed

--- blog/main.py
+++ blog/main.py
@@ -46,8 +46,8 @@ def show(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
 
     if not blog:
-        response.status = status.HTTP_404_NOT_FOUND
-        return {'detail': f"Blog with the id {id} is not available."}
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'detail': f"Blog with the id {id} is not available...."}
         # raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} is not available.")
 
     return blog
